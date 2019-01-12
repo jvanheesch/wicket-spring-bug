c95b0f53f3dfe14092534c54de2178fe83d93c94
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-12 14:40:02 +0100

    Formatting.

--- src/main/java/com/mycompany/WicketApplication.java
+++ src/main/java/com/mycompany/WicketApplication.java
@@ -6,28 +6,25 @@ import org.apache.wicket.protocol.http.WebApplication;
 /**
  * Application object for your web application.
  * If you want to run this application without deploying, run the Start class.
- * 
+ *
  * @see com.mycompany.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
-		return HomePage.class;
-	}
+public class WicketApplication extends WebApplication {
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class<? extends WebPage> getHomePage() {
+        return HomePage.class;
+    }
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
-	@Override
-	public void init()
-	{
-		super.init();
+    /**
+     * @see org.apache.wicket.Application#init()
+     */
+    @Override
+    public void init() {
+        super.init();
 
-		// add your configuration here
-	}
+        // add your configuration here
+    }
 }
